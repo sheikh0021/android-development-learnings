cfa6a7414178a1da1682c77b6de2d9a0c72cbb7b
Sheikh Rehan <165970245+sheikh0021@users.noreply.github.com>
2025-05-18 15:37:25 +0530

    Add files via upload

---
+++ Basics2.kt
@@ -0,0 +1,15 @@
+package sa.dev.kotlinbasics
+
+//
+//fun main(){
+//val myChar = '\u00AE'
+//    println(myChar)
+//}
+fun main(){
+    val myTrue: Boolean = true
+    val myFalse: Boolean = false
+
+    println(myTrue || myFalse)
+    println(myTrue && myFalse)
+
+}
