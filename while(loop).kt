085c53131bca7f78852e83c395dbb01266988a48
Sheikh Rehan <165970245+sheikh0021@users.noreply.github.com>
2025-05-19 22:18:49 +0530

    Add files via upload

---
+++ while(loop).kt
@@ -0,0 +1,13 @@
+package sa.dev.rockpaperscissorsgame
+
+
+fun main() {
+    var count = 0
+    while (count < 3){
+        //till this content is executed
+        println("Count is $count")
+        count++
+    }
+    println("Loop is completely executed")
+
+}
