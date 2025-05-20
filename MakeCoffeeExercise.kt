879b0b08fac1f63f7ffbe17b5c0a8e43b19b7b1f
Sheikh Rehan <165970245+sheikh0021@users.noreply.github.com>
2025-05-20 14:14:35 +0530

    Add files via upload

---
+++ MakeCoffeeExercise.kt
@@ -0,0 +1,24 @@
+package sa.dev.rockpaperscissorsgame
+
+
+fun main() {
+    //here we call the function that we defined
+    makeCoffee()
+
+
+
+    }
+// Here we define function
+fun makeCoffee(){
+    println("Grind beans")
+    println("pour hot water ")
+    println("mix it well enough")
+    println("Enjoy in a cup")
+
+}
+
+
+
+
+
+
