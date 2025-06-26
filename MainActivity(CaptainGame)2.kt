c3d3bd3017c1c199ebb4429a0a58421691b54e6a
Sheikh Rehan <165970245+sheikh0021@users.noreply.github.com>
2025-06-26 15:48:58 +0530

    Add files via upload

---
+++ MainActivity(CaptainGame)2.kt
@@ -0,0 +1,90 @@
+package com.wassha.captaingame
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import com.wassha.captaingame.ui.theme.CaptainGameTheme
+import kotlin.random.Random
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CaptainGameTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                    CaptainGame()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CaptainGame() {
+    val treasureFound = remember { mutableStateOf(0) }
+    val direction = remember { mutableStateOf("North") }
+    val stormOrTreasure = remember { mutableStateOf("") }
+
+    Column {
+        Text(text = "Treasure Found: ${treasureFound.value}")
+        Text(text = "Current Direction: ${direction.value}")
+        Text(text = stormOrTreasure.value)
+        Button(onClick = {
+            direction.value = "East"
+            if (Random.nextBoolean()){
+                treasureFound.value += 1
+                stormOrTreasure.value = "Found Treasure"
+            }else {
+                stormOrTreasure.value = "Storm Ahead"
+            }
+        }) {
+            Text("Sail East")
+        }
+        Button(onClick = {
+            direction.value = "West"
+            if (Random.nextBoolean()){
+                treasureFound.value += 1
+                stormOrTreasure.value = "Found Treasure"
+            } else {
+                stormOrTreasure.value = "Storm Ahead"
+            }
+        }) {
+            Text("Sail West")
+        }
+        Button(onClick = {
+            direction.value = "North"
+            if (Random.nextBoolean()){
+                treasureFound.value += 1
+                stormOrTreasure.value = "Treasure Found"
+            }else{
+                stormOrTreasure.value = "Storm Ahead"
+            }
+        }) {
+            Text("Sail North")
+        }
+        Button(onClick = {
+            direction.value = "South"
+            if (Random.nextBoolean()){
+                treasureFound.value += 1
+                stormOrTreasure.value = "Treasure Found"
+            }else {
+                stormOrTreasure.value = "Storm Ahead"
+            }
+        }) {
+            Text("Sail South")
+        }
+    }
+}
+
